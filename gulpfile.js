0ad42d2e5535d2bbd5bc788a16f6cd4be16be9d6
James Hall <llahwj33@gmail.com>
2016-04-20 09:55:48 -0400

    Development Enviroment ready.

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+// GulpFile Var
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var jade = require('gulp-jade');
+var uglify = require('gulp-uglify');
+var plumber = require('gulp-plumber');
+var prefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+
+
+gulp.task('default', ['styles', 'templates', 'server'], function(){});
+
+gulp.task('styles', function() {
+  return sass('assets/sass/main.sass')
+            .on('error', sass.logError)
+            .pipe(plumber())
+            .pipe(prefixer({
+              browser: 'last 2 versions',
+              cascade: false
+            }))
+            .pipe(gulp.dest('assets/css/'))
+            .pipe(browserSync.reload({stream: true}));
+
+});
+
+gulp.task('templates', function() {
+  return gulp.src('./**/*.jade')
+            .pipe(jade())
+            .pipe(plumber())
+            .pipe(gulp.dest('./'))
+            .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('server', function() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    },
+  });
+
+  gulp.watch('./**/*.jade', ['templates']);
+  gulp.watch('./**/*.sass', ['styles']);
+  gulp.watch('./**/*.html').on('change', browserSync.reload);
+});
